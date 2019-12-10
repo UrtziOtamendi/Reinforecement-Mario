48661fb15e172cea96e3f1eecacf77cea7e07530
Urtzi Otamendi <uotamendi@vicomtech.org>
2019-12-10 13:33:14 +0100

    Action Class with random generator

---
+++ action.py
@@ -0,0 +1,29 @@
+import numpy as np
+class Action:
+
+    '''''
+        Actions
+        
+        Six possible actions:
+                        
+            - Up
+            - Left
+            - Down
+            - Right
+            - A
+            - B
+    
+        Example:
+
+            [0,0,0,0,0,0] -> Not moving
+            [0,0,0,1,0,0] -> Moving right
+            [0,0,0,1,0,1] -> Moving right + B
+    '''''
+
+    def __init__(self):
+        self.actions=6
+
+
+    def random(self):
+        return np.random.randint(2,size=self.actions)
+
