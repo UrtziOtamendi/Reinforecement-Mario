a9d7bbc526c71039822db0378b93cb660da914ab
Urtzi Otamendi <uotamendi@vicomtech.org>
2019-12-11 10:11:28 +0100

    Policy e-gready

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from life import Life
 environment = Environment()
 
 ## Init policy 
-policy = Policy()
+max_epsilon_it=1000000
+epsilon_reduction=9e-7
+policy = Policy(max_epsilon_it,epsilon_reduction)
 
 ## Init Game controller
 game_controller= Game()
@@ -28,7 +30,7 @@ while not is_dead:
 
     while not is_done:
         # Use policy
-        action = policy.random()  # choose random action
+        action = policy.action()  # choose random action
         
         # Make an action
         observation, reward, is_done, info = environment.step(action)  # feedback from environment
