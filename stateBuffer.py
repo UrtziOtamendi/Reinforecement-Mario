85275acb16763df04a9833f2f04bbfd955b3152f
Urtzi Otamendi <uotamendi@vicomtech.org>
2019-12-13 10:20:36 +0100

    Change batch reader

--- stateBuffer.py
+++ stateBuffer.py
@@ -19,12 +19,23 @@ class StateBuffer:
 
     def batch(self,size):
         batch=[]
+        state=[]
+        next_state=[]
+        action=[]
+        reward=[]
+        done=[]
         if(self.end<size):
             return batch
         else:
             index=random.sample(range(0,self.end), size)
             for i in index:
-                batch.append(self.data[i])
+                state.append(self.data[i].start)
+                next_state.append(self.data[i].next)
+                action.append(self.data[i].action)
+                reward.append(self.data[i].reward)
+                done.append(self.data[i].is_done)
+            batch=[state,next_state,action,reward,done]
+            return batch
     
     def __getitem__(self, idx):
         return self.data[(self.start + idx) % len(self.data)]
